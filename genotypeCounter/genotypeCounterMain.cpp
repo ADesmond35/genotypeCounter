af71c663fba879d9895c47203d9205bcacdf1518
ADesmond35 <alexdesmond@knights.ucf.edu>
2019-05-09 15:44:44 -0400

    Switched inner and outer loops
    New implementation only opens one input file at a time

--- genotypeCounter/genotypeCounterMain.cpp
+++ genotypeCounter/genotypeCounterMain.cpp
@@ -8,85 +8,109 @@ using namespace std;
 #include <string>
 #define NUM_ROWS 5
 #define NUM_PATIENTS 5 // If changing NUM_PATIENTS, update list of file names at the start of main
+#define NUM_GENOTYPES 14
 
 int main() {
-	ofstream outFile;
-	outFile.open("genotypeCounts.txt", ios::out | ios::trunc);
-	outFile << "RSID\tChromosome\tPosn\tAA\tAT\tAC\tAG\tTT\tCT\tGT\tCC\tCG\tGG\tDD\tDI\tII\t--\n"; // Header line of output file
 
-	ifstream inFile[NUM_PATIENTS];
-	string fileName[NUM_PATIENTS] = {"samp1.txt","samp2.txt","samp3.txt","samp4.txt","samp5.txt"}; // All filenames go in this array. If adding or removing files, update NUM_PATIENTS
-	for (int i = 0; i < NUM_PATIENTS; i++) {
-		inFile[i].open(fileName[i]);
+	ifstream inFile;
+	string fileName[NUM_PATIENTS] = {"samp1.txt","samp2.txt","samp3.txt","samp4.txt","samp5.txt"}; // If adding or removing files, update NUM_PATIENTS
+	int counter[NUM_ROWS][NUM_GENOTYPES];
+
+	for (int row = 0; row < NUM_ROWS; row++){ // Loop through all rows (NUM_ROWS defined in preprocessor (before main))
+		for (int counterIndex = 0; counterIndex < NUM_GENOTYPES; counterIndex++){
+			counter[row][counterIndex] = 0; // Initialize all counters to 0
+		}
 	}
 
-	for (int row = 1; row <= NUM_ROWS; row++) { // Loop through all rows (NUM_ROWS defined in preprocessor (before main))
+	for (int fileNumber = 0; fileNumber < NUM_PATIENTS; fileNumber++) { // Loop through input files
+
 		string rsid, genotype; //declare string variables
 		int chromosome, position; //declare integer variables
-		int counter[14] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0}; // Initialize all counters to 0: AA, AT, AC, AG, TT, CT GT, CC, CG, GG, DD, DI, II, --
+		
+		inFile.open(fileName[fileNumber]);
 
-		for (int fileNumber = 0; fileNumber < NUM_PATIENTS; fileNumber++) { // Loop through all open input files
+		for (int row = 0; row < NUM_ROWS; row++){ // Loop through all rows (NUM_ROWS defined in preprocessor (before main))
 
-			inFile[fileNumber] >> rsid;
-			inFile[fileNumber] >> chromosome;
-			inFile[fileNumber] >> position;
-			inFile[fileNumber] >> genotype;
+			inFile >> rsid;
+			inFile >> chromosome;
+			inFile >> position;
+			inFile >> genotype;
 
-			if (genotype == "AA") {
-				counter[0]++;
+			if (genotype == "AA") { // AA, AT, AC, AG, TT, CT GT, CC, CG, GG, DD, DI, II, --
+				counter[row][0]++;
 			}
 			else if (genotype == "AT") {
-				counter[1]++;
+				counter[row][1]++;
 			}
 			else if (genotype == "AC") {
-				counter[2]++;
+				counter[row][2]++;
 			}
 			else if (genotype == "AG") {
-				counter[3]++;
+				counter[row][3]++;
 			}
 			else if (genotype == "TT") {
-				counter[4]++;
+				counter[row][4]++;
 			}
 			else if (genotype == "CT") {
-				counter[5]++;
+				counter[row][5]++;
 			}
 			else if (genotype == "GT") {
-				counter[6]++;
+				counter[row][6]++;
 			}
 			else if (genotype == "CC") {
-				counter[7]++;
+				counter[row][7]++;
 			}
 			else if (genotype == "CG") {
-				counter[8]++;
+				counter[row][8]++;
 			}
 			else if (genotype == "GG") {
-				counter[9]++;
+				counter[row][9]++;
 			}
 			else if (genotype == "DD") {
-				counter[10]++;
+				counter[row][10]++;
 			}
 			else if (genotype == "DI") {
-				counter[11]++;
+				counter[row][11]++;
 			}
 			else if (genotype == "II") {
-				counter[12]++;
+				counter[row][12]++;
 			}
 			else if (genotype == "--") {
-				counter[13]++;
+				counter[row][13]++;
 			}
 			
 		} //End of inner loop
-
+/*
 		outFile << rsid << "\t" << chromosome << "\t" << position;
 		for (int i = 0; i < 14; i++) {
 			outFile << "\t" << counter[i]; //Print all counters to output file
 		}
 		outFile << "\n";
-
+*/
+		inFile.close();
 	} // End of outer loop
 
-	for (int i = 0; i < NUM_PATIENTS; i++) {
-		inFile[i].close();
+	inFile.open(fileName[0]);
+	ofstream outFile;
+	outFile.open("genotypeCounts.txt", ios::out | ios::trunc);
+	outFile << "RSID\tChromosome\tPosn\tAA\tAT\tAC\tAG\tTT\tCT\tGT\tCC\tCG\tGG\tDD\tDI\tII\t--\n"; // Header line of output file
+
+	for (int row = 0; row < NUM_ROWS; row++){
+		string rsid, genotype; //declare string variables
+		int chromosome, position; //declare integer variables
+
+		inFile >> rsid;
+		inFile >> chromosome;
+		inFile >> position;
+		inFile >> genotype;
+		outFile << rsid << "\t" << chromosome << "\t" << position;
+
+		for (int counterIndex = 0; counterIndex < NUM_GENOTYPES; counterIndex++){
+			outFile << "\t" << counter[row][counterIndex]; //Print all counters to output file
+		}
+		outFile << "\n";
 	}
+
+	inFile.close();
 	outFile.close();
 }
